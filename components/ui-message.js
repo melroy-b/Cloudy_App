80baff4414bf8941d7d60916964e09adcb6f092b
Melroy Barreto <melroy.barreto@siemens.com>
2026-01-13 13:08:28 +0530

    updated logic for icon display

--- components/ui-message.js
+++ components/ui-message.js
@@ -1,16 +1,23 @@
 var icons = [];
 
 export default function displayImageOnUI(weatherDescription) {
-    icons.length = 0;
+    try {
+        icons.length = 0;
 
-    const UVI_INDEX = weatherDescription.uvi;
-    const weatherInfo = weatherDescription.weather[0].description;
-    const temp = weatherDescription.temp.toFixed(2);
+        const UVI_INDEX = weatherDescription.uvi;
+        const weatherInfo = weatherDescription.weather[0].description;
+        const temp = weatherDescription.temp.toFixed(2);
+        const timeOfDay = weatherDescription.weather[0].icon//new Date(weatherDescription.temp * 1000).toLocaleString("en-US", {hour: "numeric", hour12: false});
+        console.log(timeOfDay);
 
-    getUVIAdvice(UVI_INDEX)
-    getClothingAdvice({temp, weatherInfo})
+        getUVIAdvice(UVI_INDEX)
+        getClothingAdvice({temp, weatherInfo, timeOfDay})
 
-    return icons; 
+        return icons; 
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 function getUVIAdvice(UVI_INDEX) {
@@ -23,13 +30,25 @@ function getUVIAdvice(UVI_INDEX) {
     }
 }
 
-function getClothingAdvice({temp, weatherInfo}) {
-    if (temp < 15) icons.push("winter-wear");
-    if (temp < 5) icons.push("snowfall");
+function getClothingAdvice({temp, weatherInfo, timeOfDay}) {
+    const weather = weatherInfo.toLowerCase();
 
-    if (weatherInfo.includes("sunny") || weatherInfo.includes("cloud") || weatherInfo.includes("sky")) icons.push("sun");
-    if (weatherInfo.includes("rain") || weatherInfo.includes("rainy")){
-      icons.push("raincoat");  
-      icons.push("rainy");
-    } 
+    // Temperature-based icons
+    if (temp < 5) {
+    icons.push("snowfall", "winter-wear");
+    } else if (temp < 15) {
+    icons.push("winter-wear");
+    }
+
+    // Weather condition icons
+    if (weather.includes("rain")) {
+    icons.push("raincoat", "rainy");
+    } else if (weather.includes("cloud") || weather.includes("sky")) {
+    icons.push(timeOfDay.includes("d") ? "sun" : "moon");
+    }
+
+    // Air quality
+    if (weather.includes("smoke")) {
+    icons.push("mask");
+    }
 }
